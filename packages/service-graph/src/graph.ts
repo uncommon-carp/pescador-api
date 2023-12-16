73b9ddea558ba7b4f085ddec8b0b5b6dfc4a5e40
Cory Sorel <csorel.creative@gmail.com>
2023-12-16 15:19:18 -0600

    add station to schema

--- packages/service-graph/src/graph.ts
+++ packages/service-graph/src/graph.ts
@@ -1,18 +1,31 @@
-import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
-import { typeDefs } from './schema/schema';
+import { ApolloServer } from "@apollo/server";
+import {
+  startServerAndCreateLambdaHandler,
+  handlers,
+} from "@as-integrations/aws-lambda";
+import { typeDefs } from "./schema/schema";
 
 const resolvers = {
   Query: {
-    hello: () => 'world',
-    user: () => ({email: 'test@test.com', zipcode: 11111 }),
+    hello: () => "world",
+    user: () => ({ email: "test@test.com", zipCode: 11111 }),
+    station: () => ({
+      name: "Test",
+      usgsId: "01234",
+      lat: 10,
+      lon: 10,
+      values: { flow: { timestamp: "1010101", value: 10 } },
+    }),
   },
 };
 
-const server = new ApolloServer({ 
-  typeDefs, 
+const server = new ApolloServer({
+  typeDefs,
   resolvers,
   introspection: true,
 });
 
-export const graphqlHandler = startServerAndCreateLambdaHandler(server, handlers.createAPIGatewayProxyEventV2RequestHandler());
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server,
+  handlers.createAPIGatewayProxyEventV2RequestHandler(),
+);
--- packages/service-graph/src/schema/schema.ts
+++ packages/service-graph/src/schema/schema.ts
@@ -2,8 +2,29 @@ export const typeDefs = `
   type Query {
     hello: String
     user: User
+    station: Station
   }
   
+  type Station {
+    name: String!
+    usgsId: String!
+    lat: Int
+    lon: Int
+    values: ReportedValues
+  }
+
+  type ReportedValues {
+    flow: DataFrame
+    gage: DataFrame
+    height: DataFrame
+    temp: DataFrame
+  }
+
+  type DataFrame {
+    timestamp: String!
+    value: Int!
+  }
+
   type User {
     email: String!
     zipCode: Int
