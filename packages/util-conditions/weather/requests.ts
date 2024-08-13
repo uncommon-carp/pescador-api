55435300f3ef18eb5029506bf0668864bc8d9566
Cory Sorel <csorel.creative@gmail.com>
2024-08-12 23:15:25 -0500

    openWeather request rework

--- packages/util-conditions/weather/requests.ts
+++ packages/util-conditions/weather/requests.ts
@@ -1,12 +1,18 @@
 import axios from "axios";
 import { getZipCoords } from "../helpers/getZipCoords";
 
+const apiKey = process.env.OPEN_WEATHER_API_KEY;
+
 export async function requestWeatherByZip(zip: number) {
   const result = await getZipCoords(zip);
+  if (typeof result === "string") {
+    console.log("Error", result);
+    return result;
+  }
   const { lat, lng } = result;
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=30.26666&lon=-97.73330&appid=0a9d88d5d35c252b326e11f82a2b356f`,
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&appid=${apiKey}`,
     );
     return response.data;
   } catch (error) {
