f0baafc62b1d97e1c421bc30e93ba2b5324369c0
Cory Sorel <csorel.creative@gmail.com>
2024-08-15 16:56:25 -0500

    codegen for types and schema

---
+++ packages/service-graph/generator.ts
@@ -0,0 +1,21 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: ["./src/schema/schema.gql"],
+  generates: {
+    "./src/types/traits.generated.ts": {
+      plugins: ["typescript", "typescript-operations", "typescript-resolvers"],
+      config: {
+        nonOptionalTypename: true,
+        scalars: {
+          DateTime: {
+            input: "string",
+            output: "Date",
+          },
+        },
+      },
+    },
+  },
+};
+
+export default config;
